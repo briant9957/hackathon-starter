3c84b7cfeb45459e4272ab674873ad4b83c0aabf
Brian Ta <bta@squareup.com>
2021-10-02 08:22:28 +0000

    adding marker code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,54 @@ function App() {
         setLat(map.current.getCenter().lat.toFixed(4));
         setZoom(map.current.getZoom().toFixed(2));
         });
+
+      map.current.on('load', () => {
+        map.current.loadImage(
+          'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+          (error, image) => {
+            if (error) throw error;
+            map.current.addImage('custom-marker', image);
+
+            map.current.addSource('points', {
+              'type': 'geojson',
+              'data': {
+                'type': 'FeatureCollection',
+                'features': [
+                  {
+                    'type': 'Feature',
+                    'geometry': {
+                      'type': 'Point',
+                      'coordinates': [
+                        -70.9, 42.35
+                      ]
+                    },
+                    'properties': {
+                      'title': 'Mapbox DC'
+                    }
+                  }
+                ]
+              }
+            });
+
+            map.current.addLayer({
+              'id': 'points',
+              'type': 'symbol',
+              'source': 'points',
+              'layout': {
+                'icon-image': 'custom-marker',
+                // get the title name from the source's "title" property
+                'text-field': ['get', 'title'],
+                'text-font': [
+                  'Open Sans Semibold',
+                  'Arial Unicode MS Bold'
+                ],
+                'text-offset': [0, 1.25],
+                'text-anchor': 'top'
+              }
+            })
+          }
+        )
+      });
       });
 
     return (
