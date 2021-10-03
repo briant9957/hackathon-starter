750dae6e45c87dfe6a89599ae6e7ec450b9a56b0
Brian Ta <bta@squareup.com>
2021-10-03 06:47:54 +0000

    fpse

--- frontend/src/markerPopUp/index.js
+++ frontend/src/markerPopUp/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RegisterButton from '../RegisterButton';
 
 function MarkerPopUp(props) {
+    const [numberRegistered, setNumberRegistered] = useState(props.numberRegistered);
+    const handleClick = () => {
+        if (numberRegistered >= props.capacity) return;
+        const newNumberRegistered = parseInt(numberRegistered) + 1
+        setNumberRegistered(newNumberRegistered.toString());
+    }
     return (
         <div>
-            <h3>{props.title}</h3><p>{props.description}</p><p id="capacity">{props.numberRegistered}/{props.capacity}</p>
+            <h3>{props.title}</h3><p>{props.description}</p><p id="capacity">{numberRegistered}/{props.capacity}</p>
             <RegisterButton 
                 mapBoxData={props.mapBoxData}
                 setMapBoxData={props.setMapBoxData}
+                handleClick={handleClick}
             />
         </div>
     )
